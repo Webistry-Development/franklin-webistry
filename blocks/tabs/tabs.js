8879b99dac240feef60a90882b8b61e7c1b87685
Casey Tokarchuk <casey@webistry.com>
2022-11-30 17:14:48 -0500

    refactoring

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,21 +1,29 @@
+function getSection(name) {
+  const $sections = Array.from(document.querySelectorAll('.section[data-tab]'));
+  return $sections.find(($section) => $section.dataset.tab === name) ?? null;
+}
+
 export default function decorate($block) {
   const $tabs = $block.querySelectorAll('li');
-  const $cards = document.querySelectorAll('.cards-container');
-  const $cardsMinusFirst = [...$cards].splice(1); // ------> Better way to do this??/Is 'View all' an actual section or just unhide all
+  const $cards = document.querySelectorAll('section[data-tab]');
+  const $firstTab = getSection($tabs[0].textContent.trim().toLowerCase());
 
-  $cardsMinusFirst.forEach((card) => {
-    card.classList.add('hidden');
-  });
+  if ($firstTab) {
+    $firstTab.classList.add('active');
+  }
 
   $tabs.forEach((tab) => {
+    const $section = getSection(tab.textContent.trim().toLowerCase());
     tab.addEventListener('click', () => {
+      // Find currently active section
+      // Remove active class from it
+      // Find the clicked section
+      // Add the active class to it
+      $section.classList.add('active');
       const $searchTerm = tab.textContent.toLowerCase();
       $cards.forEach((card) => {
         if (card.getAttribute('data-tab').toLowerCase() === $searchTerm) {
           card.classList.remove('hidden');
-        }
-        else {
-          card.classList.add('hidden');
         }
       });
     });
