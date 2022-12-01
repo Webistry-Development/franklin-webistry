ba3db3e801c191b486e00f6db6f8065009b4d973
Casey Tokarchuk <casey@webistry.com>
2022-12-01 10:35:45 -0500

    Reworked code to work for all section types

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -1,31 +1,24 @@
 function getSection(name) {
   const $sections = Array.from(document.querySelectorAll('.section[data-tab]'));
-  return $sections.find(($section) => $section.dataset.tab === name) ?? null;
+  return $sections.find(($section) => $section.getAttribute('data-tab').toLowerCase() === name) ?? null;
 }
 
 export default function decorate($block) {
   const $tabs = $block.querySelectorAll('li');
-  const $cards = document.querySelectorAll('section[data-tab]');
-  const $firstTab = getSection($tabs[0].textContent.trim().toLowerCase());
+  const $sections = document.querySelectorAll('.section[data-tab]');
+  const $firstSection = getSection($sections[0].textContent.trim().toLowerCase());
 
-  if ($firstTab) {
-    $firstTab.classList.add('active');
-  }
+  $firstSection.classList.add('active');
 
   $tabs.forEach((tab) => {
     const $section = getSection(tab.textContent.trim().toLowerCase());
     tab.addEventListener('click', () => {
-      // Find currently active section
-      // Remove active class from it
-      // Find the clicked section
-      // Add the active class to it
-      $section.classList.add('active');
-      const $searchTerm = tab.textContent.toLowerCase();
-      $cards.forEach((card) => {
-        if (card.getAttribute('data-tab').toLowerCase() === $searchTerm) {
-          card.classList.remove('hidden');
+      $sections.forEach((section) => {
+        if (section.classList.contains('active')) {
+          section.classList.remove('active');
         }
       });
+      $section.classList.add('active');
     });
   });
 }
