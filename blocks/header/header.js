2b718804d8bce4ac0d6f04d52bae778b56dd05b7
Casey Tokarchuk <casey@webistry.com>
2022-12-08 14:30:10 -0500

    responsiveness added

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,5 +1,7 @@
 import { readBlockConfig, replaceElement } from '../../scripts/lib-franklin.js';
 
+// When you click on the hamburger give a new class to the ul that has column layout
+
 export default async function decorate($block) {
   const $cfg = readBlockConfig($block);
 
@@ -11,15 +13,14 @@ export default async function decorate($block) {
     const $container = document.createElement('div');
     $container.innerHTML = await $resp.text();
     $container.classList.add('container');
+    console.log($container.cloneNode(true));
 
     // Replace the div surrounding the Webistry logo with an a tag
     const $logoLink = document.createElement('a');
     const $logo = $container.querySelector('picture');
 
-    console.log($container.cloneNode(true));
-
     $logoLink.classList.add('header-logo');
-    $logoLink.href = 'https://www.webistry.com';
+    $logoLink.href = '/';
     $logoLink.append($logo);
     $container.querySelector('div').replaceWith($logoLink);
 
@@ -32,8 +33,8 @@ export default async function decorate($block) {
     $oldLiDiv.replaceWith($nav);
 
     // Wrap inner text of a tags with a span
-    const $list = $container.querySelectorAll('nav > ul > li > a');
-    $list.forEach((a) => {
+    const $aTags = $container.querySelectorAll('nav > ul > li > a');
+    $aTags.forEach((a) => {
       const $newSpan = document.createElement('span');
       $newSpan.textContent = a.textContent;
       a.textContent = '';
@@ -42,9 +43,28 @@ export default async function decorate($block) {
     });
 
     // Turn last li into a button
-    const $lastLi = $container.querySelector('li:last-of-type');
+    const $lastLi = $container.querySelector('strong');
     const $newCTA = replaceElement($lastLi, 'button', 'button-red');
     $newCTA.classList.add('button', 'button-small');
+
+    // hamburger for mobile
+
+    const $hamburger = document.createElement('div');
+    $hamburger.classList.add('nav-hamburger');
+    $hamburger.innerHTML = '<button class="nav-hamburger-icon"><figure class="header-hamburger">-_-</figure></button>';
+
+    const $verticalList = $container.querySelector('ul').cloneNode(true);
+    $verticalList.classList.add('header-vertical-list');
+    $container.append($verticalList);
+
+    $hamburger.addEventListener('click', () => {
+      const $list = $container.querySelector('ul');
+      $list.classList.add('showVerticalMenu');
+    });
+    $container.insertBefore($hamburger, $nav);
+   
+
+    // $nav.setAttribute('aria-expanded', 'false');
 
     $block.textContent = '';
     $block.append($container);
